702e8dcb32a40400b50803c9b3524fcfed77ade7
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-08-14 09:53:26 -0400

    added readme

--- lib/seed.py
+++ lib/seed.py
@@ -1,6 +1,6 @@
 from model import *
 
-anthony = Contact(full_name="Anthony Maddox", phone_number="15417281280", email="anthonyjmaddox@gmail.com", birthday = "1988-05-09")
+anthony = Contact(full_name="Anthony Maddox", phone_number="15417281281", email="glabra55@gmail.com", birthday = "1988-05-09")
 anthony.save()
-rebecca = Contact(full_name="Rebecca Maddox", phone_number="13018480637", email="rekercher122@gmail.com", birthday = "1988-01-22")
+rebecca = Contact(full_name="Rebecca Maddox", phone_number="13018480638", email="rekercher12@gmail.com", birthday = "1988-01-22")
 rebecca.save()
